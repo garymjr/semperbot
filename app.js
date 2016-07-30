655f98c8f0c9759178927e489faf7bdf09a05809
Gary Murray <garymjr@gmail.com>
2016-07-29 17:22:06 -0700

    broke it

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var COMMANDS = [
 
 bot.on('ready', function(event) {
 	console.log('Logged in as %s - %s\n', bot.username, bot.id);
-	console.log(bot.users.toSource())
+	console.log(bot.users)
 });
 
 bot.on('message', function(user, userID, channelID, message, event) {
