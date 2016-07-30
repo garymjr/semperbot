d328d5b5ee0465b5976de1d2a02921793406808d
Gary Murray <garymjr@gmail.com>
2016-07-30 16:18:09 -0700

    meh

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ bot.on('message', function(user, userID, channelID, message, event) {
 			});
 		} else {
 			var result = [];
-			search = message[1].lower();
+			search = message[1].toLowerCase();
 			for (i=0; i < keys.length; i++) {
 				if (keys[i].indexOf(search) !== -1) {
 					result.push(keys[i]);
