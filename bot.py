f6f9a11be70a5de39a0f3476d2a6ccf56b0a9b89
Gary Murray <garymjr@gmail.com>
2016-08-01 18:38:25 -0700

    completed migration to python

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
-from datetime import datetime
+import datetime
 import json
+import math
 
 import discord
 import asyncio
@@ -17,11 +18,14 @@ COMMANDS = [
 ]
 
 TIMES = [
-    [3, 'thursday', [19, 0], [21, 0]],
-    [5, 'saturday', [19, 0], [21, 0]],
+    # day, start, end
+    [3, [19, 0], [21, 0]],
+    [5, [19, 0], [21, 0]],
 ]
 
-GUIDES = open('guides.json')
+DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+GUIDES = 'guides.json'
 
 class SemperBot(discord.Client):
 
@@ -42,11 +46,44 @@ class SemperBot(discord.Client):
                 message.channel,
                 mention_user(message.author.id) + ' Available commands: ' + reply[:-2])
         elif content[0] == '!next':
-            pass
+            today = datetime.datetime.now()
+            raids = []
+            for raid in TIMES:
+                next_raid = datetime.datetime.now()
+                if today.weekday() != raid[0] and today.weekday() < raid[0]:
+                    days = (raid[0] - today.weekday())
+                elif today.weekday() != raid[0] and today.weekday() > raid[0]:
+                    days = (today.weekday() - raid[0]) + 7
+                elif today.weekday() == raid[0] and today.hour > raid[2][0]:
+                    days = 7
+                elif today.weekday() == raid[0] and today.hour < raid[1][0]:
+                    days = 0
+                next_raid = next_raid.replace(day=(today.day + days), hour=raid[1][0], minute=raid[1][1])
+                raids.append(next_raid)
+            raids.sort()
+            next_raid = raids.pop(0)
+            time = next_raid - today
+            days = math.floor(time.total_seconds() / 86400)
+            hours = math.floor((time.total_seconds() % 86400) / 3600)
+            minutes = math.floor(((time.total_seconds() % 86400) % 3600) / 60)
+            reply = str(days) + ' day(s), ' + str(hours) + ' hour(s), ' + str(minutes) + ' minute(s)'
+            yield from self.send_message(
+                message.channel,
+                mention_user(message.author.id) + ' The next raid is in ' + reply)
         elif content[0] == '!times':
-            pass
+            reply = ''
+            for raid in TIMES:
+                date = datetime.time(raid[1][0], raid[1][1])
+                reply += DAYS[raid[0]] + ' ' + date.strftime('%-I:%M%p - ')
+                date = datetime.time(raid[2][0], raid[2][1])
+                reply += date.strftime('%-I:%M%p (MST)') + '\n'
+            yield from self.send_message(
+                message.channel,
+                'Current raid times:\n' + reply)
         elif content[0] == '!guides':
-            keys = json.load(GUIDES)
+            file = open(GUIDES)
+            keys = json.load(file)
+            file.close()
             guides = []
             for key in keys:
                 if keys[key]['guide'] != '':
@@ -64,7 +101,9 @@ class SemperBot(discord.Client):
                     message.channel,
                     mention_user(message.author.id) + ' Available guides: ' + reply[:-2])
         elif content[0] == '!videos':
-            keys = json.load(GUIDES)
+            file = open(GUIDES)
+            keys = json.load(file)
+            file.close()
             videos = []
             for key in keys:
                 if keys[key]['video'] != '':
